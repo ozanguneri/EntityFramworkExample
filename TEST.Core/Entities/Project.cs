1156472f48095b7231afb10e0d40e44407a7e6e8
Ozan Güneri <oguneri@dsi.gov.tr>
2025-10-20 16:23:43 +0300

    Add project files.

---
+++ TEST.Core/Entities/Project.cs
@@ -0,0 +1,8 @@
+﻿namespace TEST.Core.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ TEST.Core/TestContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TEST.Core.Entities;
+
+namespace TEST.Core
+{
+    public class TestContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public TestContext(DbContextOptions<TestContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Project> Projects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = _configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseNpgsql(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>().ToTable("Project");
+        }
+    }
+}
